9243981a3c67bfb60fa87d7b2c715dc34a3515f6
teknathjha <jhateknath64@gmail.com>
2021-06-27 15:12:37 +0530

    static_root added

--- textAnalyzer/urls.py
+++ textAnalyzer/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views 
 
+# for deployment 
+form django.conf import settings 
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
     path('analyze/', views.analyze,name='analyzer'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
